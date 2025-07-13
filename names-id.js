d50554524034cea98fd3e13ed01dde8507854415
TwilightKaze <zhouxiaoran246@gmail.com>
2025-07-13 20:24:33 +0700

    Create names-id.js

---
+++ names-id.js
@@ -0,0 +1,16 @@
+// names-id.js
+export const namesID = {
+  firstNames: [
+    "Adi", "Agung", "Agus", "Ahmad", "Amin", "Andi", "Andre", "Angga", "Ari", "Arif",
+    "Bagus", "Bambang", "Bayu", "Budi", "Cahya", "Chandra", "Dede", "Dedi", "Dian",
+    "Dimas", "Eka", "Eko", "Endra", "Fajar", "Febri", "Fitri", "Hadi", "Hasan", "Hendri",
+    "Ika", "Imam", "Indah", "Intan", "Ira", "Iwan", "Joko", "Lia", "Linda", "Mega"
+  ],
+  lastNames: [
+    "Adriansyah", "Andriani", "Anggraini", "Anwar", "Arief", "Arifin", "Aziz", "Bakri",
+    "Budiman", "Cahyono", "Gunawan", "Habibi", "Hakim", "Halim", "Hamzah", "Handayani",
+
+    "Haryanto", "Hasanah", "Hermawan", "Hidayat", "Ibrahim", "Irawan", "Iskandar",
+    "Jaya", "Kurniawan", "Kusuma", "Lestari", "Maulana", "Mulyani", "Nasution"
+  ]
+};
